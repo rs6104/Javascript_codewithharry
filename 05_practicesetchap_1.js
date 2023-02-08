ea2b0d7274f54e14e28b2c14e0e5ce08b73a2ecd
RITU SINGH <107137060+rs6104@users.noreply.github.com>
2023-02-08 18:28:30 +0530

    Create 05_practicesetchap_1.js

---
+++ 05_practicesetchap_1.js
@@ -0,0 +1,31 @@
+// ques 1
+let a ="ritu "
+let b= 6
+console.log(a+b);
+
+// ques 2
+console.log(typeof (a+b));
+
+// ques 3
+const a1={
+  name:"shubham",
+  section:"b",
+  age:23,
+  isPrinciple:false
+}
+
+// a1=45;
+
+// ques 4
+a1["title"]="Singh";
+
+console.log(a1)
+
+// ques 5
+const dict={
+  boy:"male",
+  yakka:"hardwork",
+  girl:"female"
+  
+}
+console.log(dict)
