cc14c174a94c84b50cb3962d6c96cd676f75b2eb
RITU SINGH <107137060+rs6104@users.noreply.github.com>
2023-02-08 18:33:01 +0530

    Create 10_whileloop.js

---
+++ 10_whileloop.js
@@ -0,0 +1,16 @@
+//while loop
+
+ let n=prompt("enter the number..")
+ n=Number.parseInt(n);
+ let i=0;
+// while(i<n){
+//   console.log(i);
+//   i++;
+// }
+
+do{
+  console.log(i);
+  i++;
+  
+}
+  while(i<n)
