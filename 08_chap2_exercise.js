476ed6b64025544ce0b0cbefdf365ed4229aa244
RITU SINGH <107137060+rs6104@users.noreply.github.com>
2023-02-08 18:30:49 +0530

    Create 08_chap2_exercise.js

---
+++ 08_chap2_exercise.js
@@ -0,0 +1,43 @@
+//ques1
+// let age=prompt("enter your age..")
+// age=Number.parseInt(age);
+// if(age>10 && age<20){
+//   console.log("Yes")
+// }
+// else{
+//   console.log("No")
+// }
+
+//ques2
+
+/*let age=prompt("what is your age?")
+switch(age){
+  case "12":
+    console.log("your age is 12")
+    break;
+  case "13":
+    console.log("your age is 13")
+    
+    
+}*/
+
+//ques 3
+/*
+let num=prompt("enter the number..");
+num=Number.parseInt(num);
+if(num%2==0 && num%3==0){
+  console.log(num ,"is divisible by 2 and 3 ")
+}
+else{
+  console.log(num,"is not divisible by 2 and 3")
+}*/
+
+//ques 4
+
+//same as above
+
+//ques 5
+
+let age=prompt("enter you age..")
+age=Number.parseInt(age);
+console.log(age<18? "cannot drive":"can drive")
