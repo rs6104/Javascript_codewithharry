04f2ed7ad950d6a4b00cf9ef198f1b96342aeb86
RITU SINGH <107137060+rs6104@users.noreply.github.com>
2023-02-08 18:26:32 +0530

    Create 03_let_const_var.js

---
+++ 03_let_const_var.js
@@ -0,0 +1,17 @@
+console.log("diff betewwn var let and conts");
+// var a=45;
+// var b="ritu";
+// var c=null;
+// var d= undefined;
+// {
+//   let b="bittu";
+//   console.log(b);
+// }
+// console.log(b);
+
+// let b="ritu";
+// b=4;
+// console.log(b);
+
+const a="ritu";
+console.log(a);
