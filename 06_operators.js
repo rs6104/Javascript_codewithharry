1eca1e656e0ec2a0ce37552bc32f36320a48b135
RITU SINGH <107137060+rs6104@users.noreply.github.com>
2023-02-08 18:29:16 +0530

    Create 06_operators.js

---
+++ 06_operators.js
@@ -0,0 +1,41 @@
+console.log("Operators in javascript")
+let a=34;
+let b=5;
+console.log("a+b=",a+b);
+console.log("a-b=",a-b);
+console.log("a*b=",a*b);
+console.log("a/b=",a/b);
+console.log("a^b=",a**b);//power
+console.log("a%b=",a%b);//modulo
+console.log("++a=",++a);//pahle hi a ki valu ko increment kr dega
+console.log("a++=",a++);//ye baad me increment krega
+console.log("a=",a);//dekho yahan increment kiya hai bhai
+
+console.log("--a=",--a);//same chij isme bhi lagu huyi hai
+console.log("a--=",a--);
+console.log("a=",a);
+
+
+
+//Assignement operators
+
+let ass=1;
+ass+=5;
+console.log("now ass is ",ass);
+
+console.log(ass-=5)
+
+
+//Comparision operator
+
+let comp1=6;
+let comp2=5;
+console.log("comp1=comp2 is ", comp1==comp2);
+console.log("comp1!=comp2 is ", comp1!=comp2)
+
+//logical operators
+ let x=5;
+let y=6;
+console.log(x<y && x==5)
+console.log(x>y || x==5)
+console.log(!true)
