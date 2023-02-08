f3789d7d7f661aba2f3947dd9fe8afba30721fff
RITU SINGH <107137060+rs6104@users.noreply.github.com>
2023-02-08 18:27:30 +0530

    Create 04_premitives_objects.js

---
+++ 04_premitives_objects.js
@@ -0,0 +1,24 @@
+let a=null;
+let b=345
+let c=true;
+let d="ritu"
+let e=BigInt("567")+BigInt("3");
+let f=Symbol("i am a nice symbol");
+let g="undefined";
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c)
+console.log(typeof d)
+console.log(typeof e)
+console.log(typeof f)
+console.log(typeof g)
+
+//objects in javascripts/dicstonary in python
+const item={
+  "RITU": true,
+  "ramesh":false,
+  "lovish":67,
+  "rohan":undefined
+
+}
+console.log(item["RITU"])
