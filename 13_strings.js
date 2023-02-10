205266a3159812cff55fdf39cbd011f552005598
RITU SINGH <107137060+rs6104@users.noreply.github.com>
2023-02-10 09:45:28 +0530

    Create 13_strings.js

---
+++ 13_strings.js
@@ -0,0 +1,14 @@
+let name="ritu"
+console.log(name.length)
+console.log(name[0])
+console.log(name[1])
+
+
+//templete literals
+let boy1="Yash";
+let boy2="Bitto";
+console.log(`${boy1} is friend of ${boy2}` )
+
+//escape sequence characters
+let fruit='Bana\'na'
+console.log(fruit)
