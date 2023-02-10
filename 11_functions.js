7e2af510ef4c4e489e3deaa9dc339d20fff067ee
RITU SINGH <107137060+rs6104@users.noreply.github.com>
2023-02-10 09:44:00 +0530

    Create 11_functions.js

---
+++ 11_functions.js
@@ -0,0 +1,17 @@
+function oneplusavg(x,y){
+  return 1+(x+y)/2
+}
+console.log("one plus average of x and y is ", oneplusavg(2,3))
+
+console.log("one plus average of x and y is ", oneplusavg(4,3))
+
+console.log("one plus average of x and y is ", oneplusavg(5,3))
+
+
+//arrow function
+
+const sum=(a,b)=>{
+  return a+b
+  
+}
+console.log(sum(4,5));
